84eb60588f93e9a1759821ba5a7d2a1daaded062
bobby.liu <fellowe5@hotmail.com>
2024-12-31 23:44:49 +0800

    Clear db

--- start.sh
+++ start.sh
@@ -1,34 +1,28 @@
-#!/bin/bash
-
 # Project set up
 
-## Set up virtual environment
-
-### Windows
+## Set up new virtual environment
+# ---- Windows ----- #
 python -m venv env
 .\env\Scripts\activate
 
-### MacOS/Linux
+# ----- MacOS/Linux ----- #
+python -m venv env
+source venv/bin/activate
 
 # Pip install python packages
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Create Django project
-django-admin startproject HowAreYou
+# change directory to application
+cd HowAreYou/
 
-# Create application named `api`
-python manage.py startapp api
+# Seed data using management command
+python manage.py seed_db
 
-# Create admin user
-python manage.py createsuperuser
+# Run unit test
+python manage.py test
 
-
-# Run test in verbose mode
-python manage.py test -v 3
-
-
-# Admin credentials
+# Django Admin credentials
 username: admin
 password: admin123
 
